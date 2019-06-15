ead76adc2b9aae735f490b80504dea03797ea7af
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-06-15 15:34:25 +0800

    modify api route

--- js/getData.js
+++ js/getData.js
@@ -2,7 +2,7 @@
 let data = [];
 
 // fetch('http://localhost:8005/mqtt?macaddr=aa560370&startTime=2019-06-06 7:11:00&endTime=2019-06-06 12:00:00', {})
-fetch('/gps.json', {})
+fetch('../gps.json', {})
   .then((response) => {
     return response.json();
   }).then((jsonData) => {
@@ -25,7 +25,7 @@ let count = 0;
 let index = 7;
 
 // fetch('http://localhost:8005/mqtt?macaddr=aaeec755&startTime=2019-06-06 7:11:00&endTime=2019-06-06 16:00:00')
-fetch('/shake.json')
+fetch('../shake.json')
   .then((response) => {
     return response.json();
   }).then((jsonData) => {
